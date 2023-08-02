c5eefd43d7380afbb98a3f2d75b402618492c9ae
DHANUSH H V <67680433+DHANUSH-web@users.noreply.github.com>
2023-08-02 19:58:29 +0000

    Added Data Science packages

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(input("Enter your string: ")[::-1])
+"""
+Developer: Dhanush H V
+"""
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+x = np.arange(0, 200)
+y = np.random.randint(0, 100, 200)
+
+df = pd.DataFrame(dict(x=x, y=y))
+fig = px.line(df, x='x', y='y')
+fig.show()
