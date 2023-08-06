d8e7227d0547ffdfdec1f73ecff5e59a3186a14f
DHANUSH H V <dhanushhv75@gmail.com>
2023-08-07 01:50:48 +0530

    Added Data Analysis code
    Data visualization and analysis with Plotly and Pandas

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(input("Enter your string: ")[::-1])
+import plotly.express as px
+
+# data
+dataFrame = px.data.iris()
+
+# bar plot
+setosa = dataFrame[dataFrame['species'] == 'setosa']
+bar = px.scatter(data_frame=setosa, y='species', x='sepal_length', color='sepal_length', size='sepal_length', color_continuous_scale='Viridis')
+
+bar.update_layout(plot_bgcolor='white')
+bar.show()
